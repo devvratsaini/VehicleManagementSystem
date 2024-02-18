b7c01eb1b0ce9a9a0342aa769cba4ff343b6d523
Rutu Bhanderi <140520663+rutubhanderi@users.noreply.github.com>
2024-02-18 14:25:09 +0530

    Delete customer_dealer_sales.sql

--- customer_dealer_sales.sql
+++
@@ -1,7 +0,0 @@
-
-
-create table Customer(customerid int primary key,annual_income int,gender char(2) , name varchar(30),address varchar(50),dealerid int,VIN varchar(30),foreign key (dealerid)references Dealer(dealerid),foreign key (VIN)references Vehicle(VIN));
-
-create table Sales(sdate date,price int);
-
-create table Dealer(dealerid int primary key,location varchar(30),name varchar(30));
